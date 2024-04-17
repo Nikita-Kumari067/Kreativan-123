32ea1ed627c15c897ee4bab26891416c593b7b51
Nikita-Kumari067 <sahn3254@gmail.com>
2024-04-17 10:52:44 +0530

    new

--- task2/app.js
+++ task2/app.js
@@ -9,45 +9,22 @@ for(let i=1; i<=7; i++){
 
 ///////////8,64,216,512,1000
 
-for(let i=2; i<=6; i++){
-    // console.log(i*i*i);
+for(let i=2; i<=10; i=i+2){
+    
+        console.log(i*i*i);
 
-    if(i==2){
-        console.log(2**3);
 
-    }
-    else if (i==3){
-        console.log(4**3);
-    }
-    else if(i==4){
-        console.log(6**3);
-    }
-    else if(i==5){
-        console.log(8**3);
-    }
-    else{
-        console.log(10**3);
-    }
+   
 };
 
 
 /////1,4,27,16,125,36,343,64,729
-
 for(let i=1; i<=9; i++){
-    if(i===3){
-
-        console.log(i*i*i);
-    }
-    else if(i===5){
-        console.log(i*i*i);
-    }
-    else if(i===7){
-        console.log(i*i*i);
-    }
-    else if(i===9){
-        console.log(i*i*i);
-    }
-    else{
+    if(i%2==0){
         console.log(i*i);
     }
+    else{
+        console.log(i*i*i);
+    }
 }
+
