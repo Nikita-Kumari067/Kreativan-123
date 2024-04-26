eaf34eb4c2397d0b06e280ccfd6c2bea7557ad28
Nikita-Kumari067 <sahn3254@gmail.com>
2024-04-26 11:13:44 +0530

    new

---
+++ ReactDay1/myapp/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Title from "./Title.jsx";
+
+
+
+
+function App() {
+  
+  return (
+  <div>
+<Title/>
+<h1>Hello world!</h1>
+<h2>What are you doing?</h2>
+
+  </div>
+  )
+
+  
+}
+
+export default App
