52a64ac8fa92ce1842b4f2ba90f39a12e19ba8a1
Nikita-Kumari067 <sahn3254@gmail.com>
2024-05-10 12:45:34 +0530

    new

--- Weather App using react/weatherApp/src/Input.jsx
+++ Weather App using react/weatherApp/src/Input.jsx
@@ -1,33 +1,28 @@
-import {useState,useEffect } from "react"
+import {useState} from "react"
 
 export default function Input(){
     let[city, setCity]=useState(null)
     let[search, setSearch]=useState("")
-  
-
-   
     
-    
-    useEffect(()=>{
-      const getDatas=async()=>{
-        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=8439920a95672942f0b22a7de774a541`;
-          const response=await fetch(URL);
-          const data=await response.json();
-          console.log(data);
-          setCity(data.main)
-      }
-      getDatas()},[search])
-
     let handleSearch=(event)=>{
         setSearch(event.target.value)
     }
 
-    let handleSubmit=(event)=>{
+    let handleSubmit=async(event)=>{
         event.preventDefault()
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=8439920a95672942f0b22a7de774a541&units=metric`;
+        const response=await fetch(URL);
+        const data=await response.json();
+        console.log(data);
+        setCity(data)
         setSearch("")
         
-
     }
+ 
+    
+        
+
+    
 
      return(
         <div>
@@ -42,9 +37,9 @@ export default function Input(){
                 </div>
             ) : (
                 <div>
-                    <h3>Location: {search}</h3>
-                    <h3>Temperature: {city.temp}</h3>
-                    <h3>Humidity: {city.humidity}</h3>
+                    <h3>Location: {city?.name}</h3>
+                    <h3>Temperature: {city?.main?.temp}</h3>
+                    <h3>Humidity: {city?.main?.humidity}</h3>
                 </div>
             )}
             </div>
